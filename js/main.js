1b47744ab635c4035398ab6812d376bd7937f20e
mrela11 <nizam@medzhidov.ru>
2025-08-02 16:55:58 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header__active')
+        } else {
+            header.classList.remove('header__active')
+        }
+    };
+}());
+
+// Burger Hendler start 
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__nav');
+    const menuCloseItem = document.querySelector('header__nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__nav_active');
+    });
+    menuCloseItem.add('click', () => {
+        menu.classList.remove('header__nav_active');
+    });
+
+}());
